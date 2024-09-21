a4c2ffa017478c33938945ab2b1b7302a9ff4ac1
touremahi <55898676+touremahi@users.noreply.github.com>
2024-09-21 17:46:36 +0200

    templates: Dashboard, Meals, Foods, Base

--- app/routes/v1/__init__.py
+++ app/routes/v1/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from .endpoints import (
     root, login, register, dashboard,
-    foods_config
+    foods_config, meals_config
 )
 
 
@@ -12,3 +12,4 @@ web_router.include_router(login.router, prefix="/login", tags=["login"])
 web_router.include_router(register.router, prefix="/register", tags=["register"])
 web_router.include_router(dashboard.router, prefix="/dashboard", tags=["dashboard"])
 web_router.include_router(foods_config.router, prefix="/foods_configs", tags=["foods_configs"])
+web_router.include_router(meals_config.router, prefix="/meals_configs", tags=["meals_configs"])
--- app/routes/v1/endpoints/dashboard.py
+++ app/routes/v1/endpoints/dashboard.py
@@ -8,7 +8,7 @@ from app.schemas.meal import MealCreate
 from starlette.responses import RedirectResponse
 from app.db.session import get_db
 from app.services.user_service import (
-    get_user_by_id_service
+    get_user_by_id_service, get_users_service
 )
 from app.services.meal_service import (
     get_meals_by_user_service, create_meal_service
@@ -20,20 +20,16 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def dashboard(request: Request, db: Session = Depends(get_db)):
+    to_template = {
+        "request": request,
+    }
     user_id = request.cookies.get("user_id")
     if not user_id:
         return templates.TemplateResponse("login.html", {"request": request})
-    username = get_user_by_id_service(db, int(user_id)).username
-    meals = get_meals_by_user_service(db, user_id)
-
-    return templates.TemplateResponse(
-        "dashboard.html",
-        {
-            "request": request,
-            "meals": meals,
-            "username": username
-        }
-    )
+    to_template["username"] = get_user_by_id_service(db, int(user_id)).username
+    to_template["meals"] = get_meals_by_user_service(db, user_id)
+    to_template["users"] = get_users_service(db)
+    return templates.TemplateResponse("dashboard.html", to_template)
 
 @router.post("/", response_class=HTMLResponse)
 async def add_meal(
@@ -44,18 +40,17 @@ async def add_meal(
     type: str = Form(...),
     db: Session = Depends(get_db)
 ):
-    user_id = request.cookies.get("user_id")
-    if not user_id:
+    if not username:
         return templates.TemplateResponse("login.html", {"request": request})
     try:
-        get_user_by_id_service(db, int(user_id))
+        get_user_by_id_service(db, int(username))
     except ValueError as e:
         if str(e) == "User not found":
             return templates.TemplateResponse("login.html", {"request": request})
         else:
             return templates.TemplateResponse("dashboard.html", {"request": request, "error": str(e)})
     meal = MealCreate(
-        user_id=user_id,
+        user_id=username,
         date=date,
         time=time,
         type=type
@@ -63,7 +58,7 @@ async def add_meal(
     try:
         create_meal_service(db, meal)
         response = RedirectResponse(url="/dashboard", status_code=303)
-        response.set_cookie(key="user_id", value=str(user_id))
+        response.set_cookie(key="user_id", value=str(username))
         return response
     except ValidationError as e:
         return templates.TemplateResponse("dashboard.html", {"request": request, "error": str(e)})
